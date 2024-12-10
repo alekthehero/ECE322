d39d4934aaaf8b78fe727c5d264fb27bbf09250d
AlekTheHero <alekbronsky@gmail.com>
2024-12-09 23:03:19 -0500

    Lab 5 Done

--- Lab5/main.c
+++ Lab5/main.c
@@ -5,67 +5,71 @@
 #include <regex.h>
 #include <stdlib.h>
 
-int is_text_file(const char *filename) {
-    const char *ext = strrchr(filename, '.');
-    if (!ext) return 0;
-    return (strcmp(ext, ".txt") == 0 || strcmp(ext, ".c") == 0 ||
-            strcmp(ext, ".h") == 0 || strcmp(ext, ".cpp") == 0);
-}
+#define MAX_PATH 1024
+#define MAX_MATCHES 100
+#define MAX_ERROR 1024
 
-void search_file(const char *file_path, const regex_t *regex) {
+void enterFile(const char *file_path, const regex_t *regex) {
     FILE *file = fopen(file_path, "r");
     int reg;
-    regmatch_t match[100];
+    regmatch_t match[MAX_MATCHES];
     if (!file) {
         perror("fopen");
         return;
     }
 
-    char line[1024];
-    int line_number = 0;
+    char line[MAX_PATH];
+    int i = 0;
     while (fgets(line, sizeof(line), file)) {
-        line_number++;
-        reg = regexec(regex, line, 100, match, 0);
+        i++;
+        reg = regexec(regex, line, MAX_MATCHES, match, 0);
         if (reg == REG_NOMATCH) {
             continue;
         }
         if (reg != 0) {
-            char errline[1000];
-            regerror(reg, regex, errline, 1000);
+            char errline[MAX_ERROR];
+            regerror(reg, regex, errline, MAX_ERROR);
             fprintf(stderr, "%s\n", errline);
             exit(1);
         }
         if (match[0].rm_so != -1) {
-            printf("%s:%d: %s", file_path, line_number, line);
+            printf("%s:%d: %s", file_path, i, line);
         }
     }
 
     fclose(file);
 }
 
-int search_directory(const char *dir_path, const regex_t *regex) {
-    DIR *dir = opendir(dir_path);
+int isCompatFile(char *name) {
+    char *ext = strrchr(name, '.');
+    if (!ext) return 0;
+    return (strcmp(ext, ".txt") == 0 || strcmp(ext, ".c") == 0 ||
+            strcmp(ext, ".h") == 0 || strcmp(ext, ".cpp") == 0);
+}
+
+int searchDirectory(char *dirPath, const regex_t *regex) {
+    DIR *dir = opendir(dirPath);
     if (!dir) {
         perror("opendir");
         return 1;
     }
-    struct dirent *entry;
-    while ((entry = readdir(dir)) != NULL) {
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+    struct dirent *file;
+    while ((file = readdir(dir)) != NULL) {
+        if (strcmp(file->d_name, ".") == 0 || strcmp(file->d_name, "..") == 0) {
             continue;
         }
-        char full_path[1024];
-        snprintf(full_path, sizeof(full_path), "%s/%s", dir_path, entry->d_name);
+        char newPath[MAX_PATH];
+        snprintf(newPath, sizeof(newPath), "%s/%s", dirPath, file->d_name);
 
-        struct stat statbuf;
-        if (stat(full_path, &statbuf) == -1) {
+        struct stat structstat;
+        if (stat(newPath, &structstat) == -1) {
             perror("stat");
             continue;
         }
-        if (S_ISDIR(statbuf.st_mode)) {
-            search_directory(full_path, regex);
-        } else if (S_ISREG(statbuf.st_mode) && is_text_file(entry->d_name)) {
-            search_file(full_path, regex);
+        if (S_ISDIR(structstat.st_mode)) {
+            searchDirectory(newPath, regex);
+        } else if (S_ISREG(structstat.st_mode) && isCompatFile(file->d_name)) {
+            enterFile(newPath, regex);
         }
     }
 
@@ -75,21 +79,18 @@ int search_directory(const char *dir_path, const regex_t *regex) {
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        fprintf(stderr, "Usage: %s <regular expression> <directory>\n", argv[0]);
+        fprintf(stderr, "Correct: %s <regular expression> <directory>\n", argv[0]);
         return 1;
     }
     regex_t reg;
     int errcode = regcomp(&reg, argv[1], REG_EXTENDED);
     if (errcode) {
-        char errbuf[100];
+        char errbuf[MAX_ERROR];
         regerror(errcode, &reg, errbuf, sizeof(errbuf));
-        fprintf(stderr, "Could not compile reg: %s\n", errbuf);
+        fprintf(stderr, "Incorrect reg entered: %s\n", errbuf);
         return 1;
     }
-
-    search_directory(argv[2], &reg);
-
+    searchDirectory(argv[2], &reg);
     regfree(&reg);
-
     return 0;
 }
